e28125875b5297d72f2aed182c2ef1b68396970b
JAlejandroSG10 <alejito0968@gmail.com>
2024-05-26 18:24:57 -0500

    Auth with token by JWT ready OK

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const jwtPassword = 'qwe987gfd'; // Usa una clave secreta más segura en producción
-
+const jwtPassword = 'qwe987gfd'; 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   if (!authHeader) {
     return res.status(403).send('Token is required');
   }
 
-  const token = authHeader.split(' ')[1]; // Obtener el token después de 'Bearer '
+  const token = authHeader.split(' ')[1]; 
 
   if (!token) {
     return res.status(403).send('Token is required');
